215f5f9a43a3ce40a30131e5e388d3ddb69f7e0c
RGJohana <johanarangeldo@gmail.com>
2023-01-28 16:03:21 -0300

    Swagger funciona

--- contable/contable/urls.py
+++ contable/contable/urls.py
@@ -53,6 +53,4 @@ urlpatterns = [
     path('api-docs/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api-docs/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-
-
 ]
